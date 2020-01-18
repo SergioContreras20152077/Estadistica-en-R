59e45cbb85997f0ed478c7f67ea66839e3876c07
Sergio <air@MacBook-Air-de-Sergio.local>
2020-01-18 17:00:39 -0500

    cualquier cosa

--- Listas y vectores.R
+++ Listas y vectores.R
@@ -8,4 +8,12 @@ x1=c(20,32,53,56)
 x2=c("hombre", "mujer", "hombre", "mujer")
 Dataframe=data.frame(casos=x, edad=x1, sexo=x2)
 Dataframe
-
+#importar datos
+datos=read.spss("/Users/air/Downloads/Estadística en R/Employee data.sav")
+head(datos)
+library(foreign)
+summary(datos)
+datos=read.spss("/Users/air/Downloads/Estadística en R/Employee data.sav", to.data.frame = T)
+summary(datos)
+datos2=read.csv("/Users/air/Downloads/survey_sample.csv")
+summary(datos2)
