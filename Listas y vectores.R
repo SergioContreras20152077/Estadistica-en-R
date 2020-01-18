04584ee71898e629a6414b9bd1830f59f390bc8e
Sergio <air@MacBook-Air-de-Sergio.local>
2020-01-18 15:52:58 -0500

    Listas y vectores.

---
+++ Listas y vectores.R
@@ -0,0 +1,11 @@
+numeroenlista=list(1,2,3)
+numeroenlista
+vectorlista=c(1,2,3)
+vectorlista
+#se pone comillas cuando se trabaja con palabras para que R lo reconozca
+x=c("a","b","c","d")
+x1=c(20,32,53,56)
+x2=c("hombre", "mujer", "hombre", "mujer")
+Dataframe=data.frame(casos=x, edad=x1, sexo=x2)
+Dataframe
+
